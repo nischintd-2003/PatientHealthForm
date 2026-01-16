76578dcc354934c8976fae240bca4b3c5b7a87f4
nischintd-2003 <nischintd@mindfiresolutions.com>
2026-01-16 11:40:10 +0530

    feat(types): add patient type

---
+++ src/types.ts
@@ -0,0 +1,18 @@
+export interface Patient {
+  id: string;
+  name: string;
+  dob: string;
+  email: string;
+  phone: string;
+  height: string;
+  weight: string;
+  bloodType: string;
+  bp: string;
+  temp: string;
+  diseases: string[];
+  medications: string;
+  allergies: string;
+  exercise: string;
+  sleep: string;
+  diet: string;
+}

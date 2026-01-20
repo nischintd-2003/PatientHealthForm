290d65201940098de2d2179440bc64425cf0dc00
nischintd-2003 <nischintd@mindfiresolutions.com>
2026-01-20 18:18:26 +0530

    feat(header):add header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
+import { Header } from "./components/header"
 import ThemeToggle from "./components/theme-toggle"
 
 export default function App(){
     return (
         <div className="app-layout">
             <ThemeToggle/>
+            <Header/>
             <div className="main-content">
-
             </div>
         </div>
     );
---
+++ src/components/header.tsx
@@ -0,0 +1,11 @@
+export function Header() {
+    return(
+        <header className="page-header">
+            <div className="logo-icon">
+                <i className="ri-shield-cross-fill"></i>
+                <h1>Patient Health Assessment</h1>
+                <p>Please provide accurate information to help us better understand your health status.</p>
+            </div>
+        </header>
+    );
+}
